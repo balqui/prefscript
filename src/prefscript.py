7089cad113c0ce2dc0a77ca81d505c92cc813e7c
José Luis Balcázar, balqui at GitHub <jose.luis.balcazar@upc.edu>
2023-04-06 21:46:42 +0200

    Now: set up src with initial code add'ed this time

---
+++ src/prefscript.py
@@ -0,0 +1,33 @@
+'''
+PReFScript: A Partial Recursive Functions Lab
+
+Author: Jose L Balcazar, ORCID 0000-0003-4248-4528, april 2023 onwards 
+Copyleft: MIT License (https://en.wikipedia.org/wiki/MIT_License)
+
+Early steps towards a Partial Recursive Functions lab.
+
+A Python-based environment to explore and experiment with partial 
+recursive functions; naturally doubles as a (purely functional) 
+programming language, but it is not intended to be used as such.
+
+Each function in a script has associated Gödel number, internal
+name (string with the Gödel number), nickname, comments, and code.
+Gödel number stored with last operation used to construct it.
+'''
+
+import cantorpairs as cpt
+
+class PReFScript:
+
+    def __init__(self):
+        'include here the basic functions through calls to def'
+        pass
+
+    def list(what = None):
+        'if what == None: list everything, o/w search for what on the dicts'
+        pass
+
+    def define(self, how, on_what, nick, comment):
+        'add to the dicts appropriately'
+        pass
+
