4a3986f2c5294e882bf2c55b1cfaf65cbe206760
Balqui <jose.luis.balcazar@upc.edu>
2024-07-26 13:20:37 +0200

    from desktop expecting no conflict

--- parser_prag.py
+++ parser_prag.py
@@ -19,26 +19,71 @@ about = "\s*\.about(.*)\n"
 
 pragma = "\s*\.pragma\s+(\w+):?\s+(\w+)\s*"
 
-# ~ define = (start +  
-      # ~ group1_nick + 
-      # ~ group2_comment + 
-      # ~ groups6_7_pair + 
-      # ~ groups9_10_comp + 
-      # ~ groups12_13_mu)
+startdef = "\d+\s+define\:\s*"
+group1_nick = "(\w+)\s+"
+group2_comment = "\[\s*((\w|\s|[.,:;<>\)\(?\-+*]?)+)\]\s+" # add !, =, has group 4 (or is it 3?) inside
+groups5_6_how_on_what = "((pair|comp|mu)\s+(([a-zA-Z_]\w*\s+)*))" # first parenth is group 4, args required not to start with a number
 
-# ~ script = "(about | pragma | define)*"
+# ~ groups6_7_pair = "(((pair)\s+(\w+\s+\w+)\s+)"
+# ~ groups9_10_comp = "|((comp)\s+(\w+\s+\w+)\s+)"
+# ~ groups12_13_mu = "|((mu)\s+(\w+)\s+))"
 
-script = about + '|' + pragma
+define = (startdef +  
+      group1_nick + 
+      group2_comment + 
+      groups5_6_how_on_what)
+
+script = define + '|' + pragma + '|' + about
+
+for thing in re_finditer(re_compile(script), stdin.read()):
+	# ~ nick = thing.group(1)
+	# ~ if comment := thing.group(2):
+		# ~ comment = comment.strip()
+	# ~ how = thing.group(5)
+	# ~ if on_what := thing.group(6):
+		# ~ on_what = tuple(on_what.split())
+	# ~ print(str(how), str(on_what), str(nick), '/'+str(comment)+'/')
+	# ~ if g7 := thing.group(7):
+		# ~ print("in .about", g7)
+	# ~ elif g8 := thing.group(8):
+		# ~ print("in .pragma", g8, thing.group(9))
+	print("thing seen, stripped:", "-" + thing.group(0).strip() + "-")
+	for i, g in enumerate(thing.groups()):
+		print("thing group", i+1, g) 
+
+
 
 # ~ for thing in re_finditer(re_compile(about), stdin.read()):
 	# ~ print("thing seen:", thing.group(0), thing.groups())
 
-for thing in re_finditer(re_compile(script), stdin.read()):
-	if g1 := thing.group(1):
-		print("in .about", g1)
-	if g2 := thing.group(2):
-		print("in .pragma", g2, thing.group(3))
-	print("thing seen, stripped:", "-" + thing.group(0).strip() + "-")
+# ~ for thing in re_finditer(re_compile(script), stdin.read()):
+	# ~ if g16 := thing.group(16):
+		# ~ print("in .about", g16)
+	# ~ elif g14 := thing.group(14):
+		# ~ print("in .pragma", g14, thing.group(15))
+	# ~ print("thing seen, stripped:", "-" + thing.group(0).strip() + "-")
+	# ~ print("thing groups:", thing.groups())
+	# ~ else:
+		# ~ funct = thing
+		# ~ how = "no-how"
+		# ~ on_what = "no-on-what"
+	
+		# ~ nick = funct.group(1)
+		# ~ comment = funct.group(2)
+		# ~ if funct.group(7) is not None:
+			# ~ "how becomes 'pair'"
+			# ~ how = funct.group(7)
+		# ~ if funct.group(10) is not None:
+			# ~ how = funct.group(10)
+		# ~ if funct.group(13) is not None:
+			# ~ how = funct.group(13)
+		# ~ if funct.group(8) is not None:
+			# ~ on_what = tuple(funct.group(8).split())
+		# ~ if funct.group(11) is not None:
+			# ~ on_what = tuple(funct.group(11).split())
+		# ~ if funct.group(14) is not None:
+			# ~ on_what = tuple(funct.group(14).split())
+		# ~ print(how.strip(), on_what, nick.strip(), '/'+comment.strip()+'/')
 
 '''
 p0 = "(\d|\s)*define\:\s*(\w+)\s+\[\s*((\w|\s|[.,:;<>\)\(?\-+*]?)+)\]\s+(((pair)\s+(\w*\s+\w+)\s+)|((comp)\s+(\w*\s+\w+)\s+)|((mu)\s+(\w*)\s+))"
