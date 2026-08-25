59f9618aafa020bcc2a832faebd540df77080ee0
balqui <joseluisbalcazar@gmail.com>
2026-08-25 11:01:30 +0200

    Completed  CLI/pragma handling of read, write, import_folder, added intpair

---
+++ src/prefscript/praprepro.py
@@ -0,0 +1,47 @@
+'''
+Project started mid Germinal 2003:
+PReFScript: A Partial Recursive Functions Lab
+
+Module version Fructidor 2026:
+PraPrePro: pragma pre-processor, handles some CLI options from source.
+
+Author: Jose L Balcazar, ORCID 0000-0003-4248-4528
+Copyleft: MIT License (https://en.wikipedia.org/wiki/MIT_License)
+'''
+
+from re import compile as re_compile, finditer as re_finditer, MULTILINE
+
+class PraPrePro:
+    '''
+    An re-based mini-parser to pre-process pragmas before
+    sending the source to the Lark-based PReFScript parser.
+    '''
+
+    def __init__(self):
+        "group names require Python >= 3.11"
+        # ~ from re import compile as re_compile, finditer as re_finditer
+        pragma = r"^#pragma\s+(?P<which>\w+):\s*(?P<what>[\.\w]+)\s*$"
+        self.process = re_compile(pragma, MULTILINE)
+
+    def parse(self, source):
+        for thing in re_finditer(self.process, source):
+            things = thing.groupdict(default = '')
+            if which := things['which']:
+                yield which, things['what']
+
+
+if __name__ == "__main__":
+    prprpr = PraPrePro()
+    cnt = 0
+    t = '''
+# Hello World with output ascii and input none pragmas
+#pragma read: nothing
+#pragma write: ascii 
+
+main: ascii_const "Hello, World!"
+'''
+    print(t)
+    for a, b in prprpr.parse(t):
+        print(cnt, a, b)
+        cnt += 1
+    print(cnt)
--- src/prefscript/prefscript.py
+++ src/prefscript/prefscript.py
@@ -29,6 +29,7 @@ import cantorpairs as cp
 from parser import prfsparser, ScriptMaker
 from script import PReFScript
 from ascii7io import int2str
+from praprepro import PraPrePro # pragma pre-processor
 
 __version__ = "2.0"
 
@@ -42,9 +43,6 @@ ap.add_argument('filename', nargs = '?', default = None,
                 help = "file containing the script "
                       "(suffix .prfs assumed)")
 ap.add_argument('-V', '--version', action = 'version', version = f"{ap.prog} v. {__version__}")
-# ~ ap.add_argument("-C", "--cmd", 
-    # ~ help="Run as a command on stdin.",
-    # ~ action="store_true") 
 ap.add_argument("-P", "--show_parsing", 
     help = "Display the Abstract Syntax Tree of the script, don't run it.",
     action = "store_true") 
@@ -55,34 +53,45 @@ ap.add_argument("-G", "--Goedel_nums",
 
 # pragma-enabled arguments:
 
-ap.add_argument("-I", "--import_folder", # nargs = 1, 
+ap.add_argument("-I", "--import_folder", nargs = 1, 
+    default = None,               # refreshed below upon reading pragmas
     help = "additional folder where to search for imported files",
-    action = "extend")                 # should allow maybe more than one
+    action = "extend")                # should allow maybe more than one
 ap.add_argument("-R", "--read", 
-    default = "int",
-    help = "Input type format: int (default) or intseq or nothing" \
-         " (input one int or several or nothing at all)") 
+    default = None,               # refreshed below upon reading pragmas
+    help = "Input type format: int (default) or"\
+           " intpair or intseq or nothing" \
+           " (input one int or two or several or nothing at all)") 
 ap.add_argument("-W", "--write", 
-    default = "int",
+    default = None,               # refreshed below upon reading pragmas
     help = "Output type format:"\
            " int (default) or bool or ascii" \
            " (output is cast into that type)") 
 
-read, loop = pytokr.pytokr(iter = True)    # not sure why only one is insufficient
+prprpr = PraPrePro()
+
+read, loop = pytokr.pytokr(iter = True)            # why only one fails?
 
 app = ap.parse_args()
-
-import_folder = app.import_folder
-if import_folder is not None:
-    import_folder = import_folder[0]      # should clarify and refactor
-
-# ~ if app.cmd:
-    # ~ "Can't work right now due to the import-related dependency graph"
-    # ~ script_text = stdin.read()
 
 if (f := app.filename) is not None:
     with open(f) as ff:
         script_text = ff.read()
+
+    # check out pragmas and see if they are superseded by CLI args
+    pragmas = dict(prprpr.parse(script_text))
+    print(pragmas)
+    if app.read is None:
+        app.read = pragmas["read"] if "read" in pragmas else "int"
+    if app.write is None: 
+        app.write = pragmas["write"] if "write" in pragmas else "int"
+    import_folder = app.import_folder
+    if import_folder is None:
+        import_folder = pragmas["import_folder"] \
+                        if "import_folder" in pragmas else None
+    else:
+        import_folder = import_folder[0]
+
     ast = prfsparser(script_text)
     run = True
     if app.show_parsing:
@@ -96,7 +105,7 @@ if (f := app.filename) is not None:
     if run:
         "Handle first input and output formats"
         # ~ cp.ensure.that(app.read in (None, "int", "intseq", "nothing"), 
-        cp.ensure.that(app.read in ("int", "intseq", "nothing"), 
+        cp.ensure.that(app.read in ("int", "intpair", "intseq", "nothing"), 
                        f"Unknown --read value {app.read}")
         # ~ cp.ensure.that(app.write in (None, "int", "ascii", "bool"), 
         cp.ensure.that(app.write in ("int", "ascii", "bool"), 
@@ -114,6 +123,9 @@ if (f := app.filename) is not None:
                         print(outf(mainf(int(n))))
                     else:
                         break
+                case "intpair": 
+                    print(outf(mainf(cp.dp(int(read()), int(read())))))
+                    break
                 case "intseq": 
                     print(outf(mainf(cp.tup_i(map(int, loop())))))
                     break
